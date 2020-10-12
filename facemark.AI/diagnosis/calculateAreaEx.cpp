fea1bd3ae14810520519102b16545db936badcd4
jcu011 <jcu011@naver.com>
2020-10-12 19:43:32 +0900

    update calArea

--- facemark.AI/diagnosis/calculateAreaEx.cpp
+++ facemark.AI/diagnosis/calculateAreaEx.cpp
@@ -1,16 +1,9 @@
-// N개의 점 이은 면적 계산
-// 점은 순서대로 저장되어 있어야 함
-// BOJ_2166 다각형의 면적 코드
-
 #include <bits/stdc++.h>
 using namespace std;
-#define xx first
-#define yy second
-typedef long long ll;
 
 struct vector2 {
-    ll x, y;
-    vector2(ll x_=0, ll y_=0) :x(x_), y(y_){} 
+    double x, y;
+    vector2(double x_=0, double y_=0) :x(x_), y(y_){} 
     bool operator==(const vector2& jcu) const {
         return x == jcu.x && y == jcu.y;
     }
@@ -23,20 +16,30 @@ struct vector2 {
     vector2 operator-(const vector2& jcu) const {
         return vector2(x - jcu.x, y - jcu.y);
     }
-    vector2 operator*(ll jcu) const {
+    vector2 operator*(double jcu) const {
         return vector2(x * jcu, y * jcu);
     }
-    ll dot(const vector2& jcu) const {
+    double norm() const { return hypot(x,y); }
+    double dot(const vector2& jcu) const {
         return x * jcu.x + y * jcu.y;
     }
-    ll cross(const vector2& jcu) const {
+    double cross(const vector2& jcu) const {
         return x * jcu.y - jcu.x * y;
     }
 };
 
-ll ccw(vector2 p, vector2 a, vector2 b) {
-    ll ret = (a-p).cross(b-p);
+double ccw(vector2 p, vector2 a, vector2 b) {
+    double ret = (a-p).cross(b-p);
     return ret;
+}
+
+double area(const vector<vector2>&p){
+    double ret=0;
+    for(int i=0;i<p.size();i++) {
+        int j=(i+1)%p.size();
+        ret+=p[i].cross(p[j]);
+    }
+    return fabs(ret)/2.0;
 }
 
 int N;
@@ -46,17 +49,10 @@ int main() {
     cin >> N; // 점 개수
 
     for(int i=0; i<N; i++){
-        ll a,b; cin>>a>>b;
+        double a,b; cin>>a>>b;
         v.emplace_back(vector2(a,b)); // 점 좌표 저장
     }
 
-    ll res=0;
-    for(int i=1; i<N-1; i++){
-        res += ccw(v[0],v[i],v[i+1]); // ccw, 외적을 이용한 면적계산
-    }
-    
-    // 출력
-    cout << fixed; // 소수점 고정
-    cout.precision(1); // 한자리
-    cout << fabs((double)res / 2) << '\n'; // fabs(k) : double형 절대값
+    double res=area(v);
+    cout<<res;
 }
