4603b59b6ed9ce14c1f3ff39deea10252c4e34fa
suehyuny2k <66929497+suehyuny2k@users.noreply.github.com>
2020-10-27 11:55:23 +0900

    Create ParsonClass.cpp

---
+++ facemark.AI/ParsonClass.cpp
@@ -0,0 +1,31 @@
+class Parson {
+public:
+    Landmark* read(string name)
+    {
+        Landmark* Lm = new Landmark;
+        Json::Value root;
+        Json::Reader reader;
+        ifstream json(name, ifstream::binary);
+
+        reader.parse(json, root);
+        for (int i = 0; i < 68; i++)
+        {
+            Lm->all[i].x = root[to_string(i)][0].asInt();
+            Lm->all[i].y = root[to_string(i)][1].asInt();
+        }
+
+        Lm->divideMark();
+        return Lm;
+    }
+     void write(int score, int asy)
+    {
+        Json::Value root;
+        Json::Value string_value;
+
+        root["score"] = score;
+        root["asy?"] = true;
+
+        ofstream outFile("result.json", ios::out);
+        outFile << root;
+    }
+};
